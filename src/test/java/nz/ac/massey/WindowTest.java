9aad3b57fcaa5e7378e7e67de0e187377010ad3d
JamesGorman <jamie.gorman22@gmail.com>
2021-08-14 15:32:43 +1200

    Basic Testing files implemented

---
+++ src/test/java/nz/ac/massey/WindowTest.java
@@ -0,0 +1,13 @@
+package nz.ac.massey;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class WindowTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
