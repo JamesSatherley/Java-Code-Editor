1ea86fa4fdc5c0e0a1e38fe7e919d6ae123ab835
JamesGorman <jamie.gorman22@gmail.com>
2021-08-16 01:26:08 +1200

    Tests file now recognized in maven clean test

---
+++ src/test/java/nz/ac/massey/windowTest.java
@@ -0,0 +1,14 @@
+package nz.ac.massey;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class windowTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
