0636b4891b38daa77fef3d47340b4d902b62fc29
James Satherley <72740217+JamesSatherley@users.noreply.github.com>
2021-08-18 01:16:19 +1200

    Added weak saving to PDF
    - Does not conserve white space
    - Need to specify .pdf

--- src/main/java/nz/ac/massey/PrintToPDF.java
+++ src/main/java/nz/ac/massey/PrintToPDF.java
@@ -1,10 +1,31 @@
 package nz.ac.massey;
 
+import java.io.File;
+
+import javax.swing.JFileChooser;
+
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
+
 public class PrintToPDF {
+	public void print(String string) {
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setCurrentDirectory(new File("."));
 
-	public void print(String text) {
-		// TODO Auto-generated method stub
-		
+        int response = fileChooser.showSaveDialog(null);
+
+        if (response == JFileChooser.APPROVE_OPTION) {
+            try {
+                PdfWriter writer = new PdfWriter(fileChooser.getSelectedFile().getAbsolutePath());
+                PdfDocument pdf = new PdfDocument(writer);
+                Document document = new Document(pdf);
+                document.add(new Paragraph(string));
+                document.close(); 
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
 	}
-
 }
