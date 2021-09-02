1d5ddbc2a6559c7c9479ddf4ca4cc6a021a1ae60
James Satherley <72740217+JamesSatherley@users.noreply.github.com>
2021-09-03 01:19:09 +1200

    actually commented now, whoops

--- src/main/java/nz/ac/massey/OpenRFT.java
+++ src/main/java/nz/ac/massey/OpenRFT.java
@@ -4,8 +4,8 @@ import javax.swing.text.Document;
 import javax.swing.text.rtf.RTFEditorKit;
 
 public class OpenRFT {
-    RTFEditorKit rtfParser = new RTFEditorKit();
-    Document document = rtfParser.createDefaultDocument();
-rtfParser.read(new ByteArrayInputStream(rtfBytes), document, 0);
-    String text = document.getText(0, document.getLength());
+//    RTFEditorKit rtfParser = new RTFEditorKit();
+//    Document document = rtfParser.createDefaultDocument();
+//rtfParser.read(new ByteArrayInputStream(rtfBytes), document, 0);
+//    String text = document.getText(0, document.getLength());
 }
